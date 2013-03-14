1a75aa66e882e290e2b42ed121a13c5c1660c3ba
midori <dinghuahuo@gmail.com>
2013-03-14 22:35:35 +0800

    Add 1st code snippet
    Based on iOS, include image resize by resolution, save image to
    document folder.

---
+++ pictureResize.m
@@ -0,0 +1,92 @@
+//  pictureTest
+//
+//  Created by midori on 13-3-12.
+//  Copyright (c) 2013年 midori. All rights reserved.
+//
+
+- (void) main
+{
+   
+    UIImage * bigImage= [UIImage imageNamed:@"1.jpg"];
+    NSLog(@"original size: %f %f",bigImage.size.width,bigImage.size.height);
+ 
+    UIImage * scaledImage=[self resizeImageToSize:bigImage :CGSizeMake(640,480)];
+    NSLog(@"original size: %f %f",scaledImage.size.width,scaledImage.size.height);
+
+    NSString * documentsDirectoryPath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0];
+    [self saveImage:scaledImage withFileName:@"MyImage" ofType:@"PNG" inDirectory:documentsDirectoryPath];
+
+}
+
+
+-(void) saveImage:(UIImage *)image withFileName:(NSString *)imageName ofType:(NSString *)extension inDirectory:(NSString *)directoryPath {
+    if ([[extension lowercaseString] isEqualToString:@"png"]) {
+        [UIImagePNGRepresentation(image) writeToFile:[directoryPath stringByAppendingPathComponent:[NSString stringWithFormat:@"%@.%@", imageName, @"png"]] options:NSAtomicWrite error:nil];
+    } else if ([[extension lowercaseString] isEqualToString:@"jpg"] || [[extension lowercaseString] isEqualToString:@"jpeg"]) {
+        [UIImageJPEGRepresentation(image, 1.0) writeToFile:[directoryPath stringByAppendingPathComponent:[NSString stringWithFormat:@"%@.%@", imageName, @"jpg"]] options:NSAtomicWrite error:nil];
+    } else {
+        //ALog(@"Image Save Failed\nExtension: (%@) is not recognized, use (PNG/JPG)", extension);
+        NSLog(@"文件后缀不认识");
+    }
+}
+
+- (UIImage *)resizeImageToSize:(UIImage*)orgiImage:(CGSize)targetSize {
+    
+    UIImage *sourceImage = orgiImage;
+    UIImage *newImage = nil;
+    
+    CGSize imageSize = sourceImage.size;
+    CGFloat width = imageSize.width;
+    CGFloat height = imageSize.height;
+    
+    CGFloat targetWidth = targetSize.width;
+    CGFloat targetHeight = targetSize.height;
+    
+    CGFloat scaleFactor = 0.0;
+    CGFloat scaledWidth = targetWidth;
+    CGFloat scaledHeight = targetHeight;
+    
+    CGPoint thumbnailPoint = CGPointMake(0.0,0.0);
+    
+    if (CGSizeEqualToSize(imageSize, targetSize) == NO) {
+        
+        CGFloat widthFactor = targetWidth / width;
+        CGFloat heightFactor = targetHeight / height;
+        
+        if (widthFactor < heightFactor)
+            scaleFactor = widthFactor;
+        else
+            scaleFactor = heightFactor;
+        
+        scaledWidth  = width * scaleFactor;
+        scaledHeight = height * scaleFactor;
+        targetSize=CGSizeMake(scaledWidth, scaledHeight);
+        
+        // center the image
+        /*
+        if (widthFactor < heightFactor) {
+            thumbnailPoint.y = (targetHeight - scaledHeight) * 0.5;
+        } else if (widthFactor > heightFactor) {
+            thumbnailPoint.x = (targetWidth - scaledWidth) * 0.5;
+        }
+         */
+    }
+    
+    
+    UIGraphicsBeginImageContext(targetSize);
+    
+    CGRect thumbnailRect = CGRectZero;
+    thumbnailRect.origin = thumbnailPoint;
+    thumbnailRect.size.width  = scaledWidth;
+    thumbnailRect.size.height = scaledHeight;
+    
+    [sourceImage drawInRect:thumbnailRect];
+    
+    newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    if(newImage == nil) NSLog(@"could not scale image");
+    
+    
+    return newImage ;
+}
